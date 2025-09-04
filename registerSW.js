8a2535a1427fa300a5fc42fa16026cfa9cf401ea
0xvoidmain <hoangtung.utc@gmail.com>
2025-09-04 23:38:30 +0700

    .

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/swip-swip/sw.js', { scope: '/swip-swip/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
